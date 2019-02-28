07a06f62f132c86cc4d9a1a3215bbcbfec8427ae
Jasan Hernandez <jasan_azael@hotmail.es>
2019-02-28 00:44:31 -0600

    Iniciando Sistema Distribuido

---
+++ Cliente.java
@@ -0,0 +1,13 @@
+import java.rmi.Naming;
+
+public class Cliente {
+  public static void main(String []args) {
+    try {
+      System.setProperty("java.security.policy", "rmi.policy");
+      System.setSecurityManager(new SecurityManager());
+      IObjetoRemoto objrem = (IObjetoRemoto) Naming.lookup("rmi://localhost:2320/SistemasDistribuidos");
+
+      objrem.Saludo("Hola mundo desde mi casa");
+    } catch(Exception e) {  e.printStackTrace();  }
+  }
+}
